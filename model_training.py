8702d0de2350343c586d76ab614645ed23486c76
Massimo Pavoni <maspavoni@gmail.com>
2022-02-26 00:05:18 +0100

    Better model training code + saved model
    - better structure with classes and organized training flow
    - using tf nightly for GPU accelerated training to work
    - saved model after partial dataset training
    - loading model without compiling (compiling afterwards), unfortunately we lose optimizer state and other things, workaround for loading with custom object R square metric from tf addons

--- model_training.py
+++ model_training.py
@@ -1,5 +1,6 @@
+import enum
 import os
-from pickle import load as pickle_load
+from pickle import dump as pickle_dump, load as pickle_load
 from random import choices as random_choices, shuffle as random_shuffle
 
 import numpy as np
@@ -13,96 +14,184 @@ from tensorflow.python.keras.layers import (
     MaxPooling2D
 )
 from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.models import load_model as keras_load_model
 from tensorflow_addons import metrics as tfa_metrics
 
-# Deserialize data from pickle format
-with open(os.path.join('dataset', 'X.pickle'), 'rb') as pickle_in:
-    X_in = pickle_load(pickle_in)
-with open(os.path.join('dataset', 'y.pickle'), 'rb') as pickle_in:
-    y_in = pickle_load(pickle_in)
 
-# Normalize data (color values)
-X_in = X_in / 255.0
+# Dataset enum
+class Dataset(enum.Enum):
+    train = 0
+    validation = 1
+    test = 2
 
-# Oversample data
-unique, unique_index, unique_count = np.unique(y_in, return_inverse=True, return_counts=True)
-count = np.max(unique_count)
-X_oversampled = np.empty((count * len(unique) - len(X_in),) + X_in.shape[1:], X_in.dtype)
-y_oversampled = np.empty((count * len(unique) - len(y_in),) + y_in.shape[1:], y_in.dtype)
-slices = np.concatenate(([0], np.cumsum(count - unique_count)))
-for i in range(len(unique)):
-    indices = np.random.choice(np.where(unique_index == i)[0], count - unique_count[i])
-    X_oversampled[slices[i]:slices[i + 1]] = X_in[indices]
-    y_oversampled[slices[i]:slices[i + 1]] = y_in[indices]
-X_oversampled = np.concatenate((X_in, X_oversampled))
-y_oversampled = np.concatenate((y_in, y_oversampled))
 
-# Shuffle dataset
-indices = list(range(len(X_oversampled)))
-random_shuffle(indices)
-X = X_oversampled[indices]
-y = y_oversampled[indices]
+# Model class
+class OdoaldoSandwichRating:
+    def __init__(self, train_batch_size=32, epochs=10):
+        self.X = None
+        self.y = None
+        self.model = None
+        self.history = None
+        self.train_batch_size = train_batch_size
+        self.epochs = epochs
 
-# Split dataset
-X = [X_train, X_validation, X_test] = np.split(X, [int(len(X) * 0.8), int(len(X) * 0.9)])
-y = [y_train, y_validation, y_test] = np.split(y, [int(len(y) * 0.8), int(len(y) * 0.9)])
+    def __oversample_dataset(self, X_in, y_in):
+        # Oversample data
+        unique, unique_index, unique_count = np.unique(y_in, return_inverse=True, return_counts=True)
+        count = np.max(unique_count)
+        X_oversampled = np.empty((count * len(unique) - len(X_in),) + X_in.shape[1:], X_in.dtype)
+        y_oversampled = np.empty((count * len(unique) - len(y_in),) + y_in.shape[1:], y_in.dtype)
+        slices = np.concatenate(([0], np.cumsum(count - unique_count)))
+        for i in range(len(unique)):
+            indices = np.random.choice(np.where(unique_index == i)[0], count - unique_count[i])
+            X_oversampled[slices[i]:slices[i + 1]] = X_in[indices]
+            y_oversampled[slices[i]:slices[i + 1]] = y_in[indices]
+        self.X = np.concatenate((X_in, X_oversampled))
+        self.y = np.concatenate((y_in, y_oversampled))
 
-# Print dataset sizes
-print(f"Train set size: {len(X_train)}"
-      f"\nValidation set size: {len(X_validation)}"
-      f"\nTest set size: {len(X_test)}")
+    def __split_dataset(self):
+        # Split dataset
+        self.X = np.split(self.X, [int(len(self.X) * 0.8), int(len(self.X) * 0.9)])
+        self.y = np.split(self.y, [int(len(self.y) * 0.8), int(len(self.y) * 0.9)])
 
-# Show rating distribution in dataset
-fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-labels = ['train', 'validation', 'test']
-for i in range(3):
-    unique, counts = np.unique(y[i], return_counts=True)
-    axs[i].bar([idx for idx in range(len(unique))], counts)
-    axs[i].set_xticks([idx + 0.5 for idx in range(len(unique))])
-    axs[i].set_xticklabels(unique, rotation=35, ha='right', size=10)
-    axs[i].set_title(f"Rating distribution for {labels[i]} set")
-fig.tight_layout()
-plt.show()
+        # Show rating distribution in dataset
+        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+        for i in range(3):
+            unique, counts = np.unique(self.y[i], return_counts=True)
+            axs[i].bar([idx for idx in range(len(unique))], counts)
+            axs[i].set_xticks([idx + 0.5 for idx in range(len(unique))])
+            axs[i].set_xticklabels(unique, rotation=35, ha='right', size=10)
+            axs[i].set_title(f"Rating distribution for {Dataset(i).name} set ({len(self.y[i])})")
+        fig.tight_layout()
+        plt.show()
 
-# !!!!!!!!!!!!!!!! Temporary no gpu acceleration training !!!!!!!!!!!!!!!!
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    def prepare_dataset(self):
+        if not os.path.exists(os.path.join('dataset', 'X.pickle')) or not os.path.exists(
+                os.path.join('dataset', 'y.pickle')):
+            # Deserialize data from pickle format
+            with open(os.path.join('dataset', 'X_in.pickle'), 'rb') as pickle_in:
+                X_in = pickle_load(pickle_in)
+            with open(os.path.join('dataset', 'y_in.pickle'), 'rb') as pickle_in:
+                y_in = pickle_load(pickle_in)
 
-# Define model topology
-model = Sequential()
+            # Normalize data (color values)
+            X_in = X_in / 255.0
 
-model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D())
+            self.__oversample_dataset(X_in, y_in)
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D())
+            # Shuffle dataset
+            indices = list(range(len(self.X)))
+            random_shuffle(indices)
+            self.X = self.X[indices]
+            self.y = self.y[indices]
 
-model.add(Conv2D(128, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D())
+            # Serialize data into pickle format
+            with open(os.path.join('dataset', 'X.pickle'), 'wb') as pickle_out:
+                pickle_dump(self.X, pickle_out)
+            with open(os.path.join('dataset', 'y.pickle'), 'wb') as pickle_out:
+                pickle_dump(self.y, pickle_out)
 
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+            self.__split_dataset()
+        else:
+            # Deserialize data from pickle format
+            with open(os.path.join('dataset', 'X.pickle'), 'rb') as pickle_in:
+                self.X = pickle_load(pickle_in)
+            with open(os.path.join('dataset', 'y.pickle'), 'rb') as pickle_in:
+                self.y = pickle_load(pickle_in)
 
-model.summary()
+            self.__split_dataset()
 
-# Compile model
-model.compile(loss=tf_losses.MeanSquaredError(), optimizer=tf_optimizers.nadam_v2.Nadam(),
-              metrics=[tf_metrics.MeanAbsoluteError(), tfa_metrics.RSquare(y_shape=(1,))])
+    def build_model(self):
+        if not os.path.exists('odoaldo_sandwich_rating_model'):
+            # Define model architecture
+            self.model = Sequential()
 
-# Fit model
-history = model.fit(X_train, y_train, batch_size=32, epochs=5, validation_data=(X_validation, y_validation))
+            # First convolutional layer
+            self.model.add(Conv2D(32, (3, 3), input_shape=self.X[Dataset.train.value].shape[1:]))
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling2D())
 
-# Evaluate model on test set
-print("Evaluation on test data")
-results = model.evaluate(X_test, y_test, batch_size=128)
+            # Second convolutional layer
+            self.model.add(Conv2D(64, (3, 3)))
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling2D())
 
-# Show some sample evaluations
-for i in random_choices(range(len(X_test)), k=10):
-    plt.imshow(X_test[i])
-    plt.show()
-    print(model.predict(np.array([X_test[i]]))[0])
+            # Third convolutional layer
+            self.model.add(Conv2D(128, (3, 3)))
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling2D())
+
+            # Flatten layer
+            self.model.add(Flatten())
+            # Hidden fully connected layer
+            self.model.add(Dense(128))
+            self.model.add(Activation('relu'))
+            # Output layer
+            self.model.add(Dense(1))
+            self.model.add(Activation('sigmoid'))
+
+            self.model.summary()
+
+            # Compile model
+            self.model.compile(loss=tf_losses.MeanSquaredError(), optimizer=tf_optimizers.nadam_v2.Nadam(),
+                               metrics=[tf_metrics.MeanAbsoluteError(), tfa_metrics.RSquare(y_shape=(1,))])
+            return True
+        else:
+            # Load saved model
+            self.model = keras_load_model('odoaldo_sandwich_rating_model', compile=False)
+            # Recompile loaded module
+            self.model.compile(loss=tf_losses.MeanSquaredError(), optimizer=tf_optimizers.nadam_v2.Nadam(),
+                               metrics=[tf_metrics.MeanAbsoluteError(), tfa_metrics.RSquare(y_shape=(1,))])
+            return False
+
+    def train_model(self):
+        # Fit model
+        self.history = self.model.fit(self.X[Dataset.train.value], self.y[Dataset.train.value],
+                                      batch_size=self.train_batch_size, epochs=self.epochs,
+                                      validation_data=(
+                                          self.X[Dataset.validation.value], self.y[Dataset.validation.value]))
+        # Save trained model
+        self.model.save('odoaldo_sandwich_rating_model')
+
+    def evaluate_model(self):
+        # Evaluate model on test set
+        print("Evaluation on test data")
+        self.model.evaluate(self.X[Dataset.test.value], self.y[Dataset.test.value],
+                            batch_size=self.train_batch_size * 4)
+
+    def plot_model_stats_history(self):
+        # Plot model stats history
+        labels = self.history.history.keys()
+        fig, axs = plt.subplots(2, len(labels) // 2, figsize=(5 * len(labels) // 2, 12))
+        for i, key in enumerate(list(labels)):
+            row = i // (len(labels) // 2)
+            col = i % (len(labels) // 2)
+            axs[row, col].plot(range(1, self.epochs + 1), self.history.history[key])
+            axs[row, col].set_title(key)
+        fig.tight_layout(pad=4.0)
+        plt.show()
+
+    def show_random_samples(self, number_of_samples=10):
+        # Show some sample evaluations
+        for i in random_choices(range(len(self.X[Dataset.test.value])), k=number_of_samples):
+            plt.imshow(self.X[Dataset.test.value][i])
+            plt.title(f"Original label: {self.y[Dataset.test.value][i]} - "
+                      f"model prediction: {self.model.predict(np.array([self.X[Dataset.test.value][i]]))[0]}")
+            plt.show()
+
+
+def main():
+    # Use if for whatever reason you cannot train on GPU
+    # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+    odoaldo_sandwich_rating = OdoaldoSandwichRating(epochs=16)
+    odoaldo_sandwich_rating.prepare_dataset()
+    if odoaldo_sandwich_rating.build_model():
+        odoaldo_sandwich_rating.train_model()
+        odoaldo_sandwich_rating.plot_model_stats_history()
+    odoaldo_sandwich_rating.evaluate_model()
+    odoaldo_sandwich_rating.show_random_samples()
+
+
+if __name__ == '__main__':
+    main()
